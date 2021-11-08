8ea87f82d8cf80a46bb07e464b5d26e2a2fa22af
Aram <aramik22@hotmail.com>
2021-11-08 07:42:33 -0300

    Readme

--- src/routes/spotify_routes.php
+++ src/routes/spotify_routes.php
@@ -11,7 +11,6 @@ $app = new \Slim\App;
 $app->group('/v1', function () use ($app) {
     $app->get('/albums', function (Request $request, Response $response) {
         $spotify = new SpotifyController();
-        $toolkit = new ToolkitController();
         $log = new LogController();
         if (!isset($request->getQueryParams()['q'])) {
             $log->logInfo('INDEX', 'q parameter not provided');
@@ -23,7 +22,6 @@ $app->group('/v1', function () use ($app) {
     });
     $app->get('/token', function (Request $request, Response $response) {
         $spotify = new SpotifyController();
-        $toolkit = new ToolkitController();
         $response = $spotify->getAccessToken();
         return $response;
     });
